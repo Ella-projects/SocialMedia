4cf1bc1d6ccf14097167ab2522218bd801a15f79
Ella <demmanuella45@gmail.com>
2023-08-26 18:56:37 +0100

    liking posts feature

--- socialmedia/core/views.py
+++ socialmedia/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
@@ -14,6 +14,30 @@ def index(request):
 
     posts = Post.objects.all()
     return render (request, 'index.html', {'user_profile': user_profile, 'posts': posts})
+
+@login_required
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username =username).first()
+
+    if like_filter == None:
+        # new_like = LikePost(post_id=post_id, username=username)
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+
+        return redirect('/')
 
 @login_required(login_url='signin')
 def upload(request):
